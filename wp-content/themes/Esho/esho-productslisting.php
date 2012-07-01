4a0a0c8a0aed275dfd7bebad8cd475f26ffe4ea9
Emma <ragnarssonemma@hotmail.com>
2012-07-01 14:17:12 +0200

    Sidmallar

--- wp-content/themes/Esho/esho-productslisting.php
+++ wp-content/themes/Esho/esho-productslisting.php
@@ -7,6 +7,80 @@
  
  get_header(); ?>
 
-
-
+<div class="content productlist">
+		<div class="row products">
+			<div class="twelvecol">
+				 <div class="product_nav_container">
+			        <div id="product_nav_container_wide">
+			            <div class="product_container" id="product_browser">
+			                <a href="#"><img src="/wp-content/themes/esho/bilder/browser/Chrome.png"></a>
+			                <a href="#"><img src="/wp-content/themes/esho/bilder/browser/Safari.png"></a>
+			                <a href="#"><img src="/wp-content/themes/esho/bilder/browser/Firefox.png"></a>
+			                <a href="#"><img src="/wp-content/themes/esho/bilder/browser/IE.png"></a>
+			                <a href="#"><img src="/wp-content/themes/esho/bilder/browser/Maxthon.png"></a>
+			                <a href="#"><img src="/wp-content/themes/esho/bilder/browser/Opera.png"></a>
+			            </div>
+			            <div class="product_container" id="product_apple">
+			                <a href="#"><img src="/wp-content/themes/esho/bilder/apple/iMac.png"></a>
+			                <a href="#"><img src="/wp-content/themes/esho/bilder/apple/MacBook.png"></a>
+			                <a href="#"><img src="/wp-content/themes/esho/bilder/apple/MacMini.png"></a>
+			                <a href="#"><img src="/wp-content/themes/esho/bilder/apple/iPhone.png"></a>
+			                <a href="#"><img src="/wp-content/themes/esho/bilder/apple/Macpro.png"></a>
+			            </div>
+			            <div class="product_container" id="product_construction">
+			                <a href="#"><img src="/wp-content/themes/esho/bilder/construction/01.png"></a>
+			                <a href="#"><img src="/wp-content/themes/esho/bilder/construction/02.png"></a>
+			                <a href="#"><img src="/wp-content/themes/esho/bilder/construction/03.png"></a>
+			                <a href="#"><img src="/wp-content/themes/esho/bilder/construction/04.png"></a>
+			            </div>
+			             <div class="product_container" id="product_cake">
+			                <a href="#"><img src="/wp-content/themes/esho/bilder/cake/01.png"></a>
+			                <a href="#"><img src="/wp-content/themes/esho/bilder/cake/02.png"></a>
+			                <a href="#"><img src=""></a>
+			                <a href="#"><img src=""></a>
+			                <a href="#"><img src=""></a>
+			                <a href="#"><img src=""></a>
+			            </div>
+			             <div class="product_container" id="product_Linus">
+			                <a href="#"><img src=""></a>
+			                <a href="#"><img src=""></a>
+			                <a href="#"><img src=""></a>
+			            </div>
+			            <div style="clear:both"></div>
+			        </div>
+			    </div>
+			    <div class="navwrapper">
+			    	<div class="nav">
+			        	<a href="#" class="product_nav" id="nav_browser">Browser</a>
+			        	<a href="#" class="product_nav" id="nav_apple">Apple</a>
+			        	<a href="#" class="product_nav" id="nav_construction">Construction</a>
+			        	<a href="#" class="product_nav" id="nav_cake">Cake</a>
+			        	<a href="#" class="product_nav" id="nav_Linus">Linus</a>
+			    	</div>
+				</div>
+			</div>
+		</div>
+		<div class="row slider">
+			<div class="twelvecol">
+				<div id="slides">
+					<div class="slides_container">
+						<div>
+							<img src="/wp-content/themes/esho/bilder/slide1.png" alt="">
+						</div>
+						<div>
+							<img src="/wp-content/themes/esho/bilder/logo.png" alt="">
+						</div>
+						<div>
+							<img src="/wp-content/themes/esho/bilder/slide1.png" alt="">
+						</div>
+						<div>
+							<img src="/wp-content/themes/esho/bilder/slide1.png" alt="">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
 <?php get_footer(); ?>
+
--- wp-content/themes/Esho/footer.php
+++ wp-content/themes/Esho/footer.php
@@ -59,6 +59,87 @@
 
 </div><!-- #container -->
 
+<script>
+    $(document).ready(function() {
+        var displayed = "product_browser";
+        var cindex = 0; // current index (displayed product)
+
+        function loopMoveLeft(el, move) {
+            if (el.length == 1) {
+                setTimeout(function() {
+                    el.animate({ left: move }, {
+                        duration: 'slow',
+                        easing: 'easeOutBounce'
+                    });
+                    loopMoveLeft(el.next(), move);
+                }, 100);
+            }
+        }
+
+        function loopMoveRight(el, move) {
+            if (el.length == 1) {
+                setTimeout(function() {
+                    el.animate({ left: move }, {
+                        duration: 'slow',
+                        easing: 'easeOutBounce'
+                    });
+                    loopMoveRight(el.prev(), move);
+                }, 100);
+            }
+        }
+
+        function slideItem(obj_el) {
+            var product = $(obj_el).attr("id").replace("nav_", "");
+            var contid = 'product_' + product;
+
+            // current displayed element
+            var elFirst = $("#"+displayed+" a:first-child");
+            var elLast = $("#"+displayed+" a:last-child");
+            var total_el = $("#"+displayed).children().length;
+
+            var index = $(obj_el).index();
+
+            // new element to displayed
+            var el_f = $("#"+contid+" a:first-child");
+            var el_l = $("#"+contid+" a:last-child");
+            var total_new = $("#"+contid).children().length;
+
+            var movement = -960 * index;
+
+            if (cindex > index) {
+                loopMoveRight(elLast, movement); 
+                setTimeout(function() {
+                    loopMoveRight(el_l, movement) 
+                }, (total_el + 1) * 100);
+            } else if (cindex < index) {
+                loopMoveLeft(elFirst, movement); 
+                setTimeout(function() {
+                    loopMoveLeft(el_f, movement)
+                }, (total_el + 1) * 100);
+            } 
+
+            cindex = index;
+            displayed = contid;
+
+            return (total_el + 5 + total_new) * 100;
+        }
+
+        var timeout;
+
+        $(".product_nav").click(function() {
+            $(".product_nav").unbind('click');
+            timeout = slideItem(this);
+
+            // prevent abusive click
+            setTimeout(function() {
+                $(".product_nav").bind('click', function() {
+                    timeout = slideItem(this);
+                });
+            }, timeout);
+        });
+    });
+</script>
+
 <?php wp_footer(); ?>
 
 </body>
