5f3a20daef572279968b3c56b0a83d6407d6176b
Emma <ragnarssonemma@hotmail.com>
2012-07-01 11:52:52 +0200

    Skapat esho-temat

---
+++ wp-content/themes/Esho/esho-product-item.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Template Name: Esho Product Item
+ * Description: A Page Template that contains product menu, product area and banners
+ *
+ */
---
+++ wp-content/themes/Esho/esho-productslisting.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Template Name: Esho Products Listing
+ * Description: A Page Template that contains product menu, slider and banners
+ *
+ */
